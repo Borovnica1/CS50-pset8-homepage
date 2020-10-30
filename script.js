00f97d842a7c85f0a4710f6292c180188e93426f
Borovnica1 <45723338+Borovnica1@users.noreply.github.com>
2020-10-31 00:00:37 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,4 +1,6 @@
 var colors = ['red', 'blue', 'yellow', 'green', 'purple']
+var myVar;
+
 let entered = false;
 document.querySelector('.person3').addEventListener('mouseover', () => {
     const markup = `
@@ -13,14 +15,15 @@ document.querySelector('.person3').addEventListener('mouseover', () => {
     if (!entered) document.querySelector('.music-box').innerHTML = markup;
     entered = true;
 
-    var myVar = setInterval(changeColor, 1000);
+    myVar = setInterval(changeColor, 0200);
 })
 let i = 0;
 var changeColor = () => {
-    document.body.style.backgroundColor = colors[i];
+    if (i == colors.length - 1) i = 0;
+    document.documentElement.style.setProperty('--main-bg-color', colors[i]);
     i++;
 }
 
 document.querySelector('.person3').addEventListener('mouseout', () => {
-
+    clearInterval(myVar);
 })
